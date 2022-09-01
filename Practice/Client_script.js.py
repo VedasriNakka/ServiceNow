b29a8b0f8ca5f61b7144c6b6250a3b40ff075490
Nakka Vedasri <vedasri.nakka@unine.ch>
2022-09-01 17:22:28 +0200

    Client scripts

---
+++ Practice/Client_script.js.py
@@ -0,0 +1,17 @@
+function onSubmit() {
+    //Type appropriate comment here, and begin script below
+    var group = g_form.getValue('assignment_group');
+    var category = g_form.getValue('category');
+
+    if (category == 'hardware' && group == '287ebd7da9fe198100f92cc8d1d2154e') {
+        g_form.clearMessages();
+        g_form.addInfoMessage('if');
+        //g_form.submitted = false;
+        return false;
+    } else {
+        g_form.clearMessages();
+        g_form.addInfoMessage('else');
+        return true;
+    }
+
+}
