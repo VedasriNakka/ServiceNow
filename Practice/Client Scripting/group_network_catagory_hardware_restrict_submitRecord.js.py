2f124508f276971f48524947a2d6d50ab90f375f
Nakka Vedasri <vedasri.nakka@unine.ch>
2022-09-01 17:47:08 +0200

    Client scripts

--- Practice/Client Scripting/group_network_catagory_hardware_restrict_submitRecord.js.py
+++ Practice/Client Scripting/group_network_catagory_hardware_restrict_submitRecord.js.py
@@ -2,7 +2,7 @@ function onSubmit() {
     //Type appropriate comment here, and begin script below
     var group = g_form.getValue('assignment_group');
     var category = g_form.getValue('category');
-
+    
     if (category == 'hardware' && group == '287ebd7da9fe198100f92cc8d1d2154e') {
         g_form.clearMessages();
         g_form.addInfoMessage('if');
